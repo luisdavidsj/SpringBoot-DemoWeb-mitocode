a619031a1e44dac82447c1135392f7a1f4a6e36c
Luis David Saenz <luisdavidspider@gmail.com>
2021-11-10 14:52:48 -0600

    Primer commit

---
+++ DemoWeb/src/main/java/com/mitocode/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.mitocode;
+
+//change1
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.mitocode.service.UserService;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private UserService userDetailsService;
+	
+	@Autowired
+	private BCryptPasswordEncoder bcrypt;
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder;
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bcrypt);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http
+			.authorizeRequests()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.httpBasic();
+	}
+}
